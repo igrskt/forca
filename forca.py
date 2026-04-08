15302ea5bdaa663bf187b102f73d07067d54c9ec
Igor Banques <banquesigor850@gmail.com>
2026-04-08 11:14:38 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -5,11 +5,19 @@ def jogar():
 
     palavra_secreta = "banana"
 
-	enforcou = False
-	acertou = False
+    enforcou = False
+    acertou = False
 
-	while(not acertou and not enforcou):
-	    print("Jogando...")
+    while(not acertou and not enforcou):
+
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição".format(letra, index))
+            index = index + 1
+        print("Jogando...")
 
     print("Fim do jogo")
 
